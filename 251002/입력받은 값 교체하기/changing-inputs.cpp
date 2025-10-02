6225601a9e31ae021a2929630539614705919006
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-02 22:15:36 +0900

    Title: 입력받은 값 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251002/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b;
+
+    cin >> a >> b;
+    cout << b << " " << a;
+    return 0;
+}
