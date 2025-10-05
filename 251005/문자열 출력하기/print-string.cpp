9de1bbb083b39873899637b18f1992691351548a
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-05 22:03:05 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/문자열 출력하기/print-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    for(int i; i < a; i++) {
+        cout << "LeebrosCode" << endl;
+    }
+    return 0;
+}
