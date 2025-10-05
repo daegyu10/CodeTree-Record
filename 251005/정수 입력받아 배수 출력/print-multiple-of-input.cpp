a0609ce2c113dd0559a36cce1f18ffe15de41018
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-05 22:09:33 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i, a;
+    cin >> a;
+    for(i = 1; i < 6; i++) {
+        cout << a*i << " ";
+    }
+    return 0;
+}
