4965ea319529d1115a26c180f77945e0f13bb846
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-05 22:28:32 +0900

    Title: B부터 A까지 감소, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, i;
+    cin >> a >> b;
+
+    for(b = b; b >= a;) {
+        cout << b << " ";
+        b--;
+    }
+    return 0;
+}
