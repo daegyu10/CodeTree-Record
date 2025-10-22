571a85571612a8a5819616b54f94cb2383354cd2
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-22 21:31:20 +0900

    Title: 범위 안의 수, Time: 66ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251022/범위 안의 수/number-in-range.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+
+        if(10 <= a && a <= 20) {
+            Console.Write("yes");
+        } else {
+            Console.Write("no");
+        }
+    }
+}
