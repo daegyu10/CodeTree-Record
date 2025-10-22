3389291b908439bd2bf48ab818bfb5a7b3e87d59
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-22 21:47:52 +0900

    Title: 세 수의 중간값, Time: 62ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251022/세 수의 중간값/median-of-three-numbers.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split();
+
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        int c = int.Parse(input[2]);
+
+        if(b > a && b < c) {
+            Console.Write("1");
+        } else {
+            Console.Write("0");
+        }
+    }
+}
