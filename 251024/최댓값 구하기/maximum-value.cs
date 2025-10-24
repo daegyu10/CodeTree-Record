2f21bb45d12749516f2c80dcadf62363e21577b8
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-24 21:58:10 +0900

    Title: 최댓값 구하기, Time: 61ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251024/최댓값 구하기/maximum-value.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split();
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        int c = int.Parse(input[2]);
+
+        if((a >= b) && (a >= c)) {
+            Console.Write(a);
+        } else if((b >= a) && (b >= c)) {
+            Console.Write(b);
+        } else if((c >= a) && (c >= b)) {
+            Console.Write(c);
+        }
+    }
+}
