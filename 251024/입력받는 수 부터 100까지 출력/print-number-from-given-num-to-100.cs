92a67ef9a6efa918852fee9698be65de4eb5ab93
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-24 22:35:14 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 60ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251024/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = a; i <= 100; i++) {
+            Console.Write(i + " ");
+        }
+    }
+}
