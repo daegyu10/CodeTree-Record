35b2ef2b67a2769233f3eed5875d21c1a764f30b
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-25 20:54:11 +0900

    Title: 정수 입력받아 배수 출력, Time: 61ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251025/정수 입력받아 배수 출력/print-multiple-of-input.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= 5; i++) {
+            Console.Write(a*i + " ");
+        }
+    }
+}
