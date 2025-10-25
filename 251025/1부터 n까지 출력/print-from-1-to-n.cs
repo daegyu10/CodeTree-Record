becaf8de2106b2a7449551eec745dd51b855562e
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-25 20:49:27 +0900

    Title: 1부터 n까지 출력, Time: 60ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251025/1부터 n까지 출력/print-from-1-to-n.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= a; i++) { 
+            Console.Write(i + " ");
+        }
+    }
+}
