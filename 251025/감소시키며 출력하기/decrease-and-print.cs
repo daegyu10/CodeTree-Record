a55a35108a40a50e6efebb781ac9b47b44650587
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-25 21:02:50 +0900

    Title: 감소시키며 출력하기, Time: 51ms, Memory: 4MB, Status: Passed - Codetree

---
+++ 251025/감소시키며 출력하기/decrease-and-print.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        for (int i = 17; i >= 5; i--) {
+            Console.Write(i + " ");
+        }
+    }
+}
