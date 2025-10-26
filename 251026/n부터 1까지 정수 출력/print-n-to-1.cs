90c5a1909219b4fc66f7d06d07fb21b764f40ef4
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-26 22:59:49 +0900

    Title: n부터 1까지 정수 출력, Time: 59ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251026/n부터 1까지 정수 출력/print-n-to-1.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        while(a >= 1) {
+            Console.Write(a + " ");
+            a--;
+        }
+    }
+}
