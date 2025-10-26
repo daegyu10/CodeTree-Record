84f63d37c6d0947d72cdde7638b7d85916e48eba
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-26 21:28:01 +0900

    Title: 증가시키며 출력하기 2, Time: 55ms, Memory: 4MB, Status: Passed - Codetree

---
+++ 251026/증가시키며 출력하기 2/increase-and-print-2.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int i = 10;
+        while(i <= 26) {
+            Console.Write(i + " ");
+            i++;
+        }
+    }
+}
