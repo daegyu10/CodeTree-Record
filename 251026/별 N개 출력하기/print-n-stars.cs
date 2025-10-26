a3a702082ed638dc0d032bfc2961b202b3580a52
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-26 22:39:02 +0900

    Title: 별 N개 출력하기, Time: 59ms, Memory: 5MB, Status: Wrong Answer - Codetree

---
+++ 251026/별 N개 출력하기/print-n-stars.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        int i = 1;
+        while(i <= 5) {
+            Console.WriteLine("*");
+            i++;
+        }
+    }
+}
