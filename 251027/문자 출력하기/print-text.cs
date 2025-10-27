0838d3fb2be1769e7527dea2a9a359e92c67c3cd
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-27 22:26:45 +0900

    Title: 문자 출력하기, Time: 56ms, Memory: 6MB, Status: Wrong Answer - Codetree

---
+++ 251027/문자 출력하기/print-text.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        char a = char.Parse(Console.ReadLine());
+        for(int i = 1; i <= 8; i++) {
+            Console.Write("G");
+        }
+    }
+}
