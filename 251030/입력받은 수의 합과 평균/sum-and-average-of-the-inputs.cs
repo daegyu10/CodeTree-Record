3ec39bbe576d643f005b57045ad4322dacb1faf7
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-10-30 21:59:28 +0900

    Title: 입력받은 수의 합과 평균, Time: 82ms, Memory: 6MB, Status: Wrong Answer - Codetree

---
+++ 251030/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 0;
+        int b = 0;
+        int count = 0;
+        int count_num = 0;
+
+        a = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= a; i++) {
+            b = int.Parse(Console.ReadLine());
+            count += b;
+            count_num++;
+        }
+
+        Console.Write($"{count} {(double)count/count_num}");
+    }
+}
