5d8ac899906d347e992c66a1f20869f05e1f5ecd
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-06 20:18:39 +0900

    Title: 별표 출력하기 7, Time: 60ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251106/별표 출력하기 7/print-star-7.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 0; i < a; i++) {
+            for(int j = 0; j <= i; j++) { 
+                Console.Write("* ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
