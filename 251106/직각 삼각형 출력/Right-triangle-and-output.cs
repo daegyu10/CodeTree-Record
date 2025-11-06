be77b66f66a953ea90c744103aea836526d4f22a
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-06 20:28:28 +0900

    Title: 직각 삼각형 출력, Time: 60ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251106/직각 삼각형 출력/Right-triangle-and-output.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 0; i < a; i++) {
+            for(int j = 0; j < (i*2)+1; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
