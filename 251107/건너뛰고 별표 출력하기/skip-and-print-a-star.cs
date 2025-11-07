4f2eca846857e7cda042ee8ad1389db6fffb04dc
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-07 22:10:51 +0900

    Title: 건너뛰고 별표 출력하기, Time: 60ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251107/건너뛰고 별표 출력하기/skip-and-print-a-star.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        int count = 1;
+        for(int i = 0; i < (a*2)-1; i++) {
+            for(int j = 0; j < count; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            if(i >= a-1) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+    }
+}
