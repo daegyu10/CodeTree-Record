b2bc6a8b265ba01554a184c1368378e6644bbdd5
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-13 17:50:45 +0900

    Title: 숫자 피라미드 3, Time: 62ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251113/숫자 피라미드 3/number-pyramid-3.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= a; i++) {
+            for(int j = 1; j <= i; j++) {
+                Console.Write(i*j + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
