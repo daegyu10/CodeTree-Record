31db7bf502acf23031bf9da603ccffbc52968a53
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-13 17:56:55 +0900

    Title: 모양대로 수 출력하기, Time: 62ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251113/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+
+        for(int i = a; i > 0; i--) {
+            for(int j = a; j > 0; j--) {
+                if(i >= j) {
+                    Console.Write(j + " ");
+                } else {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
