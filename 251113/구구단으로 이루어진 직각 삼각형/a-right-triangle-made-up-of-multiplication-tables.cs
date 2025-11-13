1a9e7b5692a35b1fd75a68f3bde500909a16978e
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-13 18:23:15 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 63ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251113/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= a; i++) {
+            for(int j = 1; j <= (a-i+1); j++) {
+                if(j == (a-i+1)) {
+                    Console.Write($"{i} * {j} = {i*j}");
+                } else {
+                    Console.Write($"{i} * {j} = {i*j} / ");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
