4e9537f926391d5eb34335336e5d1d4151f181a8
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-13 17:48:09 +0900

    Title: 숫자 피라미드, Time: 63ms, Memory: 6MB, Status: Passed - Codetree

---
+++ 251113/숫자 피라미드/number-pyramid.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= a; i++) {
+            for(int j = 0; j < i; j++) {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
