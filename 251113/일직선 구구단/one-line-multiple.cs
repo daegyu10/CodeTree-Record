0eec7bc66f5ba513ab5882619952d89dfc35f13c
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-13 18:11:27 +0900

    Title: 일직선 구구단, Time: 61ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251113/일직선 구구단/one-line-multiple.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= a; i++) {
+            for(int j = 1; j <= a; j++) {
+                Console.WriteLine($"{i} * {j} = {i*j}");
+            }
+        }
+    }
+}
