7991bfb1fb30a9604a9d929a04fd548f2d395368
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-14 21:16:08 +0900

    Title: 정사각 알파벳 출력하기, Time: 62ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251114/정사각 알파벳 출력하기/print-alpabet-in-square.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        int b = 'A';
+
+        for(int i = 1; i <= a; i++) {
+            for(int j = 1; j <= a; j++) {
+                Console.Write((char)b);
+                b++;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
