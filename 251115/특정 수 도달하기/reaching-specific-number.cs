c3ac4cfd619b30e3b2bb90bf813e06aa4e6c4984
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-15 22:46:21 +0900

    Title: 특정 수 도달하기, Time: 82ms, Memory: 5MB, Status: Passed - Codetree

--- 251115/특정 수 도달하기/reaching-specific-number.cs
+++ 251115/특정 수 도달하기/reaching-specific-number.cs
@@ -6,25 +6,27 @@ public class Codetree
     {
         // Please write your code here.
         int[] arr = new int[10];
-        var input = Console.ReadLine().Split(' ');
-
-        arr = Array.ConvertAll(input, int.Parse);
-
         int sum = 0;
         double avg = 0.0;
         int count = 0;
+        
+        string[] input = Console.ReadLine().Split();
+
+        for(int i = 0; i < 10; i++) {
+            arr[i] = int.Parse(input[i]);
+        }
+
 
         for(int i = 0; i < 10; i++) {
             if(arr[i] >= 250) {
                 break;
             } 
-            //Console.Write(i);
             sum += arr[i];
-            avg += (double)arr[i];
             count++;
         }
 
-        Console.Write($"{sum} {avg/count:F1}");
+        avg = (double)sum / count;
+        Console.Write($"{sum} {avg:F1}");
     }
 }
 
