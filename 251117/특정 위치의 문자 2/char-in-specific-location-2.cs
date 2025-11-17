558f17db86e5b92b9713184588421282835a2f2b
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-17 23:55:36 +0900

    Title: 특정 위치의 문자 2, Time: 55ms, Memory: 4MB, Status: Passed - Codetree

---
+++ 251117/특정 위치의 문자 2/char-in-specific-location-2.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split();
+        
+        Console.Write(input[1] + " " + input[4] + " " + input[7]);
+    }
+}
