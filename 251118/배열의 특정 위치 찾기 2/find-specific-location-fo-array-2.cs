95dc8909561dc29f5b08187cd68aa40bd25cbe10
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-18 00:13:20 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 62ms, Memory: 8MB, Status: Passed - Codetree

---
+++ 251118/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int[] arr = new int[10];
+        string[] input = Console.ReadLine().Split();
+
+        int sum_odd = 0;
+        int sum_even = 0;
+
+        for(int i = 0; i < 10; i++) {
+            arr[i] = int.Parse(input[i]);
+            if(i % 2 == 1) {
+                sum_even += arr[i];
+            } else {
+                sum_odd += arr[i];
+            }
+        }
+
+        if(sum_even >= sum_odd) {
+            Console.Write(sum_even - sum_odd);
+        } else {
+            Console.Write(sum_odd - sum_even);
+        }
+
+    }
+}
