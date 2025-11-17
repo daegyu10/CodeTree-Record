8d36eff6c9ea8121f42204860c9145a718dc8e64
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-17 23:57:19 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 96ms, Memory: 7MB, Status: Runtime Error - Codetree

--- 251117/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.cs
+++ 251117/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.cs
@@ -1,26 +1,28 @@
 using System;
 
 public class Codetree
-{  
+{
     public static void Main()
     {
-        // Please write your code here.
+        // 변수 선언 (k : 0이 들어있는 인덱스의 번호)
+        int[] arr = new int[100];
+        int k = 0;
+
+        // 최대 100개의 정수를 배열에 입력받아 저장합니다.
         string[] input = Console.ReadLine().Split();
+        arr = Array.ConvertAll(input, int.Parse);
 
-        int save_size = input.Length;
-        int[] save = new int[save_size];
-        
-        for(int i = 0; i < save_size; i++) {
-            save[i] = int.Parse(input[i]);
-        }
-
-        for(int i = 0; i < save_size; i++) {
-            if(save[i] == 0) {
-                Console.Write(save[i-3] + save[i-2] + save[i-1]);
+        // 0이 입력된 위치를 확인합니다.
+        for (int i = 0; i < 100; i++)
+        {
+            if (arr[i] == 0)
+            {
+                k = i;
                 break;
             }
         }
 
-        
+        // 출력
+        Console.WriteLine((arr[k - 3] + arr[k - 2] + arr[k - 1]).ToString());
     }
 }
