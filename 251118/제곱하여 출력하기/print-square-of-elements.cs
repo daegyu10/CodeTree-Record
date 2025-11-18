5484ae2e686926dd3b8300f47120bccf7d2ec3bb
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-18 23:48:59 +0900

    Title: 제곱하여 출력하기, Time: 62ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251118/제곱하여 출력하기/print-square-of-elements.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[a];
+        string[] input = Console.ReadLine().Split();
+        for(int i = 0; i < a; i++) {
+            arr[i] = int.Parse(input[i]);
+            Console.Write(arr[i] * arr[i] + " ");
+        }
+    }
+}
