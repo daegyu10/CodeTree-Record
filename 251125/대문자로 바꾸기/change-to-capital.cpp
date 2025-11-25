5869c39eb451a4522846d3190940dd1f3781a1b9
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-25 23:44:28 +0900

    Title: 대문자로 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251125/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char arr[5][3] = {};
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+            arr[i][j] = arr[i][j] -= 32;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
