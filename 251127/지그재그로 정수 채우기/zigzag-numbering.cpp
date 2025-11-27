ab838fd36033555d672c60a1681828e608f12f8d
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-11-27 22:05:54 +0900

    Title: 지그재그로 정수 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/지그재그로 정수 채우기/zigzag-numbering.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    int num = 0;
+    int arr[n][m] = {};
+
+    for(int i = 0; i < m; i++) {
+        if(i % 2 == 0) {
+            for(int j = 0; j < n; j++) {
+                arr[j][i] = num;
+                num++;
+            }
+        } else {
+            for(int j = n-1; j >= 0; j--) {
+                arr[j][i] = num;
+                num++;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
