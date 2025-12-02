883184def95f6e960af9c10477fcfe5f1a49ddca
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-02 22:47:38 +0900

    Title: 단어로 구분하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251202/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string arr[10];
+    for(int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < 10; i+=2) {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
