2dd2a8b0b0623e4f136ebc7020543a586ab6df5d
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-03 20:46:47 +0900

    Title: 문자열에서 문자 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251203/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    for(int i = 0; i < str.length(); i++) {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
