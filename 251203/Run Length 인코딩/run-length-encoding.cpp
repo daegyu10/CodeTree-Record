b2b541dbfc3382f9ec45dadb448d54988c2cf862
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-03 21:30:23 +0900

    Title: Run Length 인코딩, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251203/Run Length 인코딩/run-length-encoding.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+int main() {
+    cin >> A;
+
+    // Please write your code here.
+    string str;
+    int count = 1;
+    
+    str += A[0];
+    for(int i = 0; i < A.length() - 1; i++) {
+        if(A[i] == A[i+1]) {
+            count++;
+        } else if(A[i] != A[i+1]) {
+            str += to_string(count);
+            count = 1;
+            str += A[i+1];
+        }
+        if(i == A.length() -2) {
+            str += to_string(count);
+        }
+    }
+
+    cout << str.length() << endl << str;
+    return 0;
+}
