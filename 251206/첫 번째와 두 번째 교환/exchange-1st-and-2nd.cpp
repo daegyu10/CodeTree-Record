4777176cfc29cfa17cd77fb3a538402b81091c02
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-06 23:32:43 +0900

    Title: 첫 번째와 두 번째 교환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251206/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    char first = str[0];
+    char second = str[1];
+
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == first) {
+            str[i] = second;
+        } else if(str[i] == second) {
+            str[i] = first;
+        }
+    }
+
+    cout << str;
+    return 0;
+}
