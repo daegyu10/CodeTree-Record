0d19dc169a9b7c8398a84df0faf87a8422d5e3c5
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-09 23:01:05 +0900

    Title: 아스키코드 변환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    char b;
+
+    cin >> b >> a;
+
+    cout << (int)b << " " << (char)a;
+    return 0;
+}
