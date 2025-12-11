8b33bef3f8b02e61f4bf8fa8873e27d0168a1648
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-11 22:32:11 +0900

    Title: 정수만 추출하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str_a;
+    string str_b;
+    cin >> str_a >> str_b;
+
+    cout << stoi(str_a)+stoi(str_b);
+    return 0;
+}
