816ab27644a72e21888ed35563617f9d52d8470b
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-11 22:49:36 +0900

    Title: 정수만 추출하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251211/정수만 추출하기/extract-only-integers.cpp
+++ 251211/정수만 추출하기/extract-only-integers.cpp
@@ -7,6 +7,25 @@ int main() {
     string str_b;
     cin >> str_a >> str_b;
 
-    cout << stoi(str_a)+stoi(str_b);
+    string trans_a;
+    string trans_b;
+    for(int i = 0; i < str_a.length(); i++) {
+        if('0' <= str_a[i] && str_a[i] <= '9') {
+            trans_a += str_a[i];
+        } else {
+            break;
+        }
+    }
+
+    for(int i = 0; i < str_b.length(); i++) {
+        if('0' <= str_b[i] && str_b[i] <= '9') {
+            trans_b += str_b[i];
+        } else {
+            break;
+        }
+    }
+
+    cout << stoi(trans_a)+stoi(trans_b);
+    
     return 0;
 }
