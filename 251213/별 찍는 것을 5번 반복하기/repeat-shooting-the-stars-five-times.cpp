dcc0d4ba6e62103eacde74a1d9f2081630229ee9
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-13 22:07:26 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251213/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Print10Star() {
+        for(int i = 0; i < 10; i++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+int main() {
+    // Please write your code here.
+    for(int i = 0; i < 5; i++) {
+        Print10Star();
+    }
+    return 0;
+}
