eda1be48fae64c6e771972624ef376acde4eff41
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-13 22:15:32 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251213/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void Printline(int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    Printline(N);
+    return 0;
+}
