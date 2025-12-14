bf66690ab7cddbd907ade61ede33722bb7920bc2
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2025-12-14 22:12:14 +0900

    Title: 최대공약수 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251214/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+void Maxdiv(int n, int m) {
+    int save;
+    int max;
+    if(n > m) {
+        max = n;
+    } else {
+        max = m;
+    }
+
+    for(int i = 1; i < max; i++) {
+        if(n % i == 0 && m % i == 0) {
+            save = i;
+        }
+    }
+
+    cout << save;
+}
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    Maxdiv(n,m);
+    return 0;
+}
