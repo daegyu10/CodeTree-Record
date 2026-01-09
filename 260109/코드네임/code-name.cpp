2a02598086ec71b37ef6c38bfb34978914c36962
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-09 23:55:25 +0900

    Title: 코드네임, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260109/코드네임/code-name.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Record {
+    public:
+        char name;
+        int score;
+
+        Record(char name = 'z', int score = 0) {
+            this->name = name;
+            this->score = score;
+        }
+};
+
+int main() {
+    // Please write your code here.
+    char a;
+    int b;
+    int min = 0;
+    int index;
+
+    Record records[5];
+    for(int i = 0; i < 5; i++) {
+        cin >> a >> b;
+        records[i] = Record(a,b);
+    }
+
+    Record record0 = records[0];
+    min = record0.score;
+
+    for(int i = 1; i < 5; i++) {
+        record0 = records[i];
+        if(min > record0.score) {
+            min = record0.score;
+            index = i;
+        }
+    }
+
+    Record record1 = records[index];
+    cout << record1.name << " " << record1.score;
+    return 0;
+}
