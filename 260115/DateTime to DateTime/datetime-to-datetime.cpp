2216f20dd3ff8cb775bedd09354a0af5d2c8e62b
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-15 21:15:25 +0900

    Title: DateTime to DateTime, Time: 1099ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260115/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int d1 = 11;
+    int h1 = 11;
+    int m1 = 11;
+
+    int t_d1, t_h1, t_m1;
+
+    cin >> t_d1 >> t_h1 >> t_m1;
+    int n_minute = 0;
+
+    while(true) {
+        if(d1 == t_d1 && h1 == t_h1 && m1 == t_m1) {
+            break;
+        }
+
+        m1++;
+        n_minute++;
+
+        if(m1 >= 60) {
+            h1++;
+            m1 = 0;
+        }
+
+        if(h1 >= 24) {
+            d1++;
+            h1 = 0;
+        }
+    }
+
+    cout << n_minute;
+    return 0;
+}
