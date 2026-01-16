6d77d2aeea2a214275351fab0e282cff361ffe10
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-16 22:07:44 +0900

    Title: 그 요일은, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260116/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int num_Days(int m, int d) {
+    int m_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int total_days = 0;
+
+    for(int i = 1; i < m; i++) {
+        total_days += m_days[i];
+    }
+
+    total_days += d;
+    return total_days;
+}
+
+int main() {
+    // Please write your code here.
+    int now_m, now_d;
+    int tar_m, tar_d;
+    string day;
+    cin >> now_m >> now_d >> tar_m >> tar_d >> day;
+    
+    int diff = num_Days(tar_m, tar_d) - num_Days(now_m, now_d);
+
+    int count = diff / 7;
+    int last_num = diff % 7;
+
+    string day_arr[7] = {"Mon","Sun","Sat","Fri","Thu","Wed","Tue"};
+    for(int i = 0; i <= last_num; i++) {
+        if(day == day_arr[i]) {
+            count++;
+        }
+    }
+
+    cout << count;
+    return 0;
+}
