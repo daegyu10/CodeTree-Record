92737436adcf7d3a1417c95e77909404ff814910
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-19 20:11:47 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260119/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int arr[200] = {}; //음수는 배열 인덱스로 포함 불가. 따라서 -100~+100.
+
+    cin >> n;
+    int index = 100; //한 가운데. 숫자로 치면 0.
+    for(int i = 0; i < n; i++) {
+        int a;
+        char c;
+        cin >> a >> c;
+        if(c == 'R') {
+            for(int i = 0; i < a; i++) {
+                arr[index]++;
+                index++;
+            }
+        } else {
+            for(int i = 0; i < a; i++) {
+                arr[index]++;
+                index--;
+            }
+        }
+    }
+
+    int count = 0;
+    for(int i = 0; i < 200; i++) {
+        if(arr[i] >= 2) {
+            count++;
+        }
+    }
+
+    cout << count;
+    return 0;
+}
