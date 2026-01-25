27005a08dffb4091f63006e9aed4d639421b9cef
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-25 23:23:11 +0900

    Title: 계속 중첩되는 사각형, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260125/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int arr[201][201] = {}; //항상 명심. 초기화 안하면 쓰레기값 들어간다.
+
+    for(int i = 0; i < n; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for(int i = x1+100; i < x2+100; i++) {
+            for(int j = y1+100; j < y2+100; j++) {
+                arr[i][j] = 1;
+            }
+        }
+
+        cin >> x1 >> y1 >> x2 >> y2; //파란색. 빨강은 1, 파랑은 2.
+        for(int i = x1+100; i < x2+100; i++) {
+            for(int j = y1+100; j < y2+100; j++) {
+                arr[i][j] = 2;
+            }
+        }
+    }
+
+    int count = 0;
+    for(int i = 0; i < 201; i++) {
+        for(int j = 0; j < 201; j++) {
+            if(arr[i][j] == 2) {
+                count++;
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
