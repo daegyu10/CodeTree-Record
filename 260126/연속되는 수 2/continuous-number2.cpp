b10979b1fcc9e3a2a524fae73e7357d4ab72ab5e
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-26 21:40:51 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260126/연속되는 수 2/continuous-number2.cpp
+++ 260126/연속되는 수 2/continuous-number2.cpp
@@ -8,13 +8,21 @@ int main() {
     int arr[n] = {};
 
     int count = 0;
+    int max_count = 0;
+
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
         if(i == 0 || arr[i] != arr[i-1]) {
+            count = 1;
+        } else if(arr[i] == arr[i-1]) {
             count++;
+        }
+
+        if(max_count < count) {
+            max_count = count;
         }
     }
 
-    cout << count;
+    cout << max_count;
     return 0;
 }
