c64cbfb57d1cc53a20553a020c7c52111d20c51a
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-27 19:59:09 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260127/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
+++ 260127/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -12,7 +12,7 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
-        if(i >= 1 && arr[i] > t) {
+        if(arr[i] > t) {
             count++;
         } else {
             count = 0;
