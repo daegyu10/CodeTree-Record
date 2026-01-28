07fd2298a351e64c5d2bacda5b45303269da61bb
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-01-28 20:14:18 +0900

    Title: 벌금은 누구에게, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260128/벌금은 누구에게/who-will-pay.cpp
+++ 260128/벌금은 누구에게/who-will-pay.cpp
@@ -7,13 +7,14 @@ int main() {
     int pun_m;
     int limit_k;
     cin >> stu_n >> pun_m >> limit_k;
-    int stu[stu_n] = {};
+    int stu[stu_n+1] = {};
 
     for(int i = 0; i < pun_m; i++) {
         int num;
         cin >> num;
-        stu[num-1]++;
-        if(stu[num-1] == limit_k) {
+        stu[num]++;
+
+        if(stu[num] == limit_k) {
             cout << num;
             return 0;
         }
