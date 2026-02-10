88957266d813b8f7f669e4b7d0899d53f27bee4e
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-02-10 23:15:00 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260210/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+bool InRange(int a, int b) {
+    return (0 <= a && a < n && 0 <= b && b < n);
+}
+int main() {
+    // Please write your code here.
+    cin >> n;
+    int arr[n][n] = {};
+    int visited[n][n] = {};
+
+    string command;
+    int a;
+    cin >> a >> command;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int x, y;
+    x = n / 2;
+    y = n / 2;
+    visited[x][y] = 1;
+
+    int dx[4] = {0, 1, 0, -1}; //동 남 서 북
+    int dy[4] = {1, 0, -1, 0};
+    int dir_num = 3;
+
+    for(int i = 0; i < a; i++) {
+        if(command[i] == 'L') {
+            dir_num = (dir_num + 3) % 4;
+        } else if(command[i] == 'R') {
+            dir_num = (dir_num + 1) % 4;
+        } else if(command[i] == 'F') {
+            int nx = x + dx[dir_num];
+            int ny = y + dy[dir_num];
+            if(InRange(nx, ny)) {
+                x = nx;
+                y = ny;
+                visited[x][y] = 1;
+            } else {
+                x = x;
+                y = y;
+            }
+        }
+    }
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(visited[i][j] == 1) {
+                sum += arr[i][j];
+            }
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
