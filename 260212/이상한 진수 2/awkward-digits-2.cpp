a824969b53423d186a1223a16b9e5d9d9ad43db0
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-02-12 23:56:25 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260212/이상한 진수 2/awkward-digits-2.cpp
+++ 260212/이상한 진수 2/awkward-digits-2.cpp
@@ -6,7 +6,24 @@ int main() {
     string input;
     cin >> input;
 
-    if(input.size() == 1) {
+    int answer = 0;
+    int n = (int)input.size();
+    for(int i = 0; i < n; i++) {
+        input[i] = '0' + '1' - input[i];
+        int number = 0;
+        //i번째 자리를 1<->0을 바꾸고, 그걸 10진수로 바꿈. 즉, 모든 자리에서 하나만 바꿔보는 것.
+        for(int j = 0; j < n; j++) {
+            number = number * 2 + (input[i] - '0');
+        }
+        answer = max(answer, number);
+        input[i] = '0' + '1' - input[i]; //1<->0 바꾼거 다시 원래대로 바꾸기.
+    }
+    cout << answer;
+    return 0;
+}
+
+/*
+if(input.size() == 1) {
         input = '0';
     } else {
         for(int i = 0; i < (int)input.size(); i++) {
@@ -30,5 +47,5 @@ int main() {
     }
 
     cout << answer;
-    return 0;
-}
+
+*/
