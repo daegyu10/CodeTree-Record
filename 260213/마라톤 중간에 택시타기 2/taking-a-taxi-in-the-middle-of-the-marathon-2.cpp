178e40601a0f70f8919489c15c8d5adbb65af7e9
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-02-13 21:04:46 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260213/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
+++ 260213/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -15,17 +15,36 @@ int main() {
         cin >> x[i] >> y[i];
     }
 
-    int dist = 0;
-    for(int i = 0; i < n; i++) { //시작, 끝은 냅두고 중간에 하나만 체크포인트를 지워야함.
+    int dist_min = INT_MAX;
+    //시작, 끝은 냅두고 중간에 하나만 체크포인트를 지워야함.
     //그러면 그 체크포인트를 스킵하는 로직을 짜야하는데 이를 어떻게 할 것인가?
-        for(int j = 1; j < n-1; j++) { //이동거리 구하려면 다 돌아야한다. 그 중에서 j에 해당하는 체크포인트만 생략하게.
-            if(i != j) { //근데 그러면 1번째 체크포인트 생략하면 2번째를, 2번째 생략하면 1번째 포인트를 선택하게 해야한다. 그건?
-            //따로 bool 배열 넣어서 확인해야하나?
-                dist += (abs(x[]))
+
+    //이동거리 구하려면 다 돌아야한다. 그 중에서 j에 해당하는 체크포인트만 생략하게.
+    //근데 그러면 1번째 체크포인트 생략하면 2번째를, 2번째 생략하면 1번째 포인트를 선택하게 해야한다. 그건?
+    //배열을 따로 만든다. 그래서 i랑 j랑 같으면 그건 패스하고 append. 그리고 그걸 이동거리 구해서 최소거리.
+    for(int j = 1; j < n-1; j++) {
+        int dist = 0;
+        int skip_x[n-1] = {};
+        int skip_y[n-1] = {};
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(i != j) {
+                skip_x[count] = x[i];
+                skip_y[count] = y[i];
+                count++;
             }
         }
 
+        for(int i = 0; i < n-2; i++) {
+            dist += (abs(skip_x[i] - skip_x[i+1]) + abs(skip_y[i] - skip_y[i+1]));
+        }
 
+        if(dist < dist_min) {
+            dist_min = dist;
+        }
     }
+
+    cout << dist_min;
     return 0;
 }
