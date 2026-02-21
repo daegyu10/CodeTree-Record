b0dce1371b3ac38894fe377afb55dabad92a28ec
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-02-21 23:59:28 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260221/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 260221/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -13,13 +13,13 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> can_num;
         cin >> position;
-        arr[position-1] = can_num;
+        arr[position-1] += can_num;
     }
 
     int max_num = INT_MIN;
-    for(int i = 0; i < 100-2*k; i++) {
+    for(int i = 0; i <= 100-2*k; i++) {
         int count = 0;
-        for(int j = i; j <= i+(2*k); j++) {
+        for(int j = i; j < i+(2*k); j++) {
             count += arr[j];
         }
         max_num = max(max_num, count);
