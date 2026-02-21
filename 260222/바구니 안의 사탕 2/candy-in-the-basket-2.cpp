d542bb4d833676993adf86afc62dbba04fbdd073
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-02-22 00:05:26 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260222/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#include <climits>
+
+int main() {
+    // Please write your code here.
+    int n, k;
+    cin >> n >> k;
+
+    int can_num;
+    int position;
+    int arr[100] = {};
+    for(int i = 0; i < n; i++) {
+        cin >> can_num;
+        cin >> position;
+        arr[position-1] += can_num;
+    }
+
+    int max_num = INT_MIN;
+    for(int i = 0; i < 100-2*k; i++) { //i로 잡는 범위는 100-2*k여야 함. k로 중간점을 잡으면 범위 크기는 항상 홀수임.
+    //k가 3이다. 그럼 1 ~ 7이 가능하다. 즉 1,2,3,4,5,6,7. 7개다. 따라서 i는 <=가 아니라 <를 써야한다. 그래야 93 94 95 96 97 98 99. 이렇게 된다.
+        int count = 0;
+        for(int j = i; j <= i+(2*k); j++) {
+            count += arr[j];
+        }
+        max_num = max(max_num, count);
+    }
+
+    cout << max_num;
+    return 0;
+}
