6efe18c4504f291c695e427e270b088db903df0f
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-02 21:25:33 +0900

    Title: 겹치지 않는 선분 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260302/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 260302/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -15,21 +15,18 @@ int main() {
     //겹치는 조건이 무엇일까. start i번째와 end i번째 사이에 start, end j가 들어가면 겹친다.
     //그걸 알아보자.
     int count = 0;
-    int check[n] = {};
     for(int i = 0; i < n; i++) {
+        bool cross = false;
         for(int j = 0; j < n; j++) { //검사 과정은 쌍방으로 간다. 그러니까 4개 있는데 겹치는게 있으면 선분은 2개 겹친다. 그림으로 보면 단순.
         //겹치는 조건은 2개다. a가 b보다 왼쪽에서 출발했는데, 오른쪽에 도착하는 것. 혹은 a가 b보다 오른쪽에서 출발했는데 왼쪽에 도착하는것.
         //1번째는 1,5 / 2,3 (이러면 겹친다) 2번째는 3,2 / 5,1. 이러면 겹친다.
         //겹치는 조건 설계를 잘못했음.
             if((start[i] < start[j] && end[i] > end[j]) || (start[i] > start[j] && end[i] < end[j])) {
-                check[i] = 1;
-                check[j] = 1;
+                cross = true;
+                break;
             }
         }
-    }
-
-    for(int i = 0; i < n; i++) {
-        if(check[i] != 1) {
+        if(cross == false) {
             count++;
         }
     }
