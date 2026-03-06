dd785f76a6863fd571354942ca53c67ebd4f81a3
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-06 23:34:23 +0900

    Title: 선분 3개 지우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260306/선분 3개 지우기/remove-three-segments.cpp
+++ 260306/선분 3개 지우기/remove-three-segments.cpp
@@ -6,36 +6,32 @@ int main() {
     // Please write your code here.
     int n;
     cin >> n;
-    int arr[101] = {};
-    int arr_2[101] = {};
+
     int a[10] = {};
     int b[10] = {};
     int start, end;
 
     for(int i = 0; i < n; i++) {
         cin >> a[i] >> b[i];
-        start = a[i];
-        end = b[i];
-        for(int j = start; j <= end; j++) {
-            arr[j]++;
-        }
     }
 
     int count = 0;
     for(int i = 0; i < n-2; i++) {
         for(int j = i+1; j < n-1; j++) {
             for(int k = j+1; k < n; k++) {
-                copy(arr, arr+101, arr_2);
+
+                int arr_2[101] = {};
 
                 for(int l = 0; l < n; l++) {
                     if(l == i || l == j || l == k) {
+                        continue;
+                    }
                         start = a[l];
                         end = b[l];
                         for(int m = start; m <= end; m++) {
-                            arr_2[m]--;
+                            arr_2[m]++;
                         }
                     }
-                }
 
                 for(int m = 0; m < 101; m++) {
                     if(arr_2[m] > 1) {
@@ -44,7 +40,6 @@ int main() {
                         count++;
                     }
                 }
-
             }
         }
     }
