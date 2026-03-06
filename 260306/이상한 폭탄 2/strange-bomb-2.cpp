7c0cedea940fe823fdb71e6ae69d55f21a6f5f52
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-06 22:05:13 +0900

    Title: 이상한 폭탄 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260306/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, k;
+    cin >> n >> k;
+
+    int arr[n] = {};
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int num = -1;
+    int max_num = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(arr[i] == arr[j] && abs(i-j) <= k) {
+                num = arr[i];
+            }
+            max_num = max(max_num, num);
+        }
+    }
+
+    cout << max_num;
+    return 0;
+}
