531d9d984e90a6034d1346a1551f3de4cd317bc4
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-22 22:00:07 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260322/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    char arr[n] = {};
+    string line;
+    
+    cin >> line;
+    for(int i = 0; i < n; i++) {
+        arr[i] = line[i];
+    }
+
+    //비교를 해야한다. 굳이 새로운 배열 만들 필요 없을듯.
+    //첫번째 for문에선 시작점. 두번재 for문에선 length 변수로 arr[i] 잡고 비교. 
+    //지금 봐야할 건, 1. 길이. 2. 비교할 문자열의 시작 장소. 3. 시작점 이후 비교할(기존 문자열) 문자열.
+    //c++에서, 문자열에는 특정 부분 문자열이 있는걸 찾아주는 find 함수가 있다. str.find(str2) != string::npos -> 즉, 이건 있다는 것
+    for(int i = 1; i < n; i++) { //길이
+        bool exist = false;
+
+        for(int j = 0; j < n-i; j++) { //시작점
+            string str;
+            for(int k = j; k < j+i; k++) { //도착점.
+                str += arr[k];
+            }
+
+            if(line.find(str, j+i) != string::npos) {
+                exist = true;
+                break;
+            }
+        }
+
+        if(!exist) {
+            cout << i;
+            return 0;
+        }
+    }
+    
+    return 0;
+}
