c5a4ec18260378d0f340a440f156a5b11516b0a1
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-24 23:56:37 +0900

    Title: 가장 작은 x 찾기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260324/가장 작은 x 찾기/find-smallest-x.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int arr1[n] = {};
+    int arr2[n] = {};
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr1[i] >> arr2[i];
+    }
+    for(int i = 1; i < 1000; i++) { //첫 시작 수.
+        int work = i;
+        for(int j = 0; j < n; j++) {
+            work *= 2;
+            if(!(arr1[j] <= work && work <= arr2[j])) {
+                break;
+            } else if(j == n-1) {
+                cout << i;
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
