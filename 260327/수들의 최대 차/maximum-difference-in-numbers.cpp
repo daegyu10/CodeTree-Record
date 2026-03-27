d0a3361bd6f1f4efc5defcee251ac45e04a9a656
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-27 23:14:14 +0900

    Title: 수들의 최대 차, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260327/수들의 최대 차/maximum-difference-in-numbers.cpp
+++ 260327/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -15,9 +15,9 @@ int main() {
 
     int max_count = 0;
     for(int i = 0; i < n; i++) {
-        int count = 1; //스스로를 골랐으니 count는 1.
-        for(int j = i+1; j < n; j++) {
-            if(abs(arr[i]-arr[j]) <= k) {
+        int count = 0; //스스로를 골랐으니 count는 1.
+        for(int j = 0; j < n; j++) { //수를 다 골라본다. 1,2,3,4,5. 이 상태에서 3을 기준으로 4,5 하는거랑 5를 기준으로 보는거랑 다를 수 있음.
+            if((i != j) &&(abs(arr[i]-arr[j]) <= k)) { //스스로가 아니고 k 이하면 플러스.
                 count++;
             }
         }
