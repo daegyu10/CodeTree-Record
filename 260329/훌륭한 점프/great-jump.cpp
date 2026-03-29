a8bc6b00e198a27cd2adbbbff02e4c3b782c899c
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-03-29 21:02:39 +0900

    Title: 훌륭한 점프, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260329/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int n, k;
+
+bool Possible(int a) {
+    int count = 0;
+    int indexes[n] = {};
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] >= a) {
+            indexes[count++] = i;
+        }
+    }
+
+    for(int i = 1; i < count; i++) {
+        int distance = indexes[i] - indexes[i-1];
+        if(distance > k) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    // Please write your code here.
+
+    cin >> n >> k;
+    int max_count = 0;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(max_count < arr[i]) {
+            max_count = arr[i];
+        }
+    }
+
+    //조건을 어떻게 해야할끼? 일단 최대값을 구하고. 그거에서 최소값을 구하고. 그 와중에서 거리를 봐야한다.
+    //아까 개념대로, 정답을 가정해보자. 
+    int minmax = 100000;
+    for(int i = 1; i <= max_count; i++) {
+        if(Possible(i)) {
+            minmax = min(minmax, i);
+        }
+    }
+
+    cout << minmax;
+    return 0;
+}
