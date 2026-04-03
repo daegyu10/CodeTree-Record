d4c1b0e5da9822fed6d79ce67dec2350e70d8895
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-04-03 21:13:15 +0900

    Title: 이상한 폭탄 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260403/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, k;
+    cin >> n >> k;
+    int arr[n] = {};
+    bool index[1000001] = {}; 
+    int index2[n] = {};
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(index[arr[i]] == false) {
+            index2[count++] = arr[i];
+            index[arr[i]] = true;
+        }
+    }
+
+    int explode_max = 0;
+    for(int i = 0; i < count; i++) {
+        int target = index2[i];
+        int idx = 0;
+        bool first = false;
+        int explode = 0;
+
+        for(int j = 0; j < n; j++) {
+            if(arr[j] == target) {
+                if(first == false) {
+                    first = true;
+                    idx = 0;
+                } else {
+                    if(idx <= k) {
+                        explode++;
+                        idx = 0;
+                    }
+                }
+            } else {
+                if(first == true) {
+                    idx++;
+                }
+            }
+
+        }
+
+        explode_max = max(explode_max, explode);
+    }
+    cout << explode_max;
+
+    return 0;
+}
