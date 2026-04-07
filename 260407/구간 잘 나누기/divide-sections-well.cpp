3543a6f2c2a26fc3239c6e5814af9ba3476c4dee
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-04-07 23:16:30 +0900

    Title: 구간 잘 나누기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260407/구간 잘 나누기/divide-sections-well.cpp
+++ 260407/구간 잘 나누기/divide-sections-well.cpp
@@ -13,8 +13,10 @@ int main() {
     for(int i = 1; i <= 100000; i++) {
         int count = 0;
         int sector_count = 0;
+        bool exist = true;
         for(int j = 0; j < n-1; j++) {
             if(arr[j] > i) {
+                exist = false;
                 break;
             }
             count += arr[j];
@@ -27,7 +29,7 @@ int main() {
             }
         }
 
-        if(sector_count == m-1) {
+        if(exist == true && sector_count == m-1) {
             cout << i;
             return 0;
         }
