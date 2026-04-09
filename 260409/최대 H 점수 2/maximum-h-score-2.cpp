718b03cd7c09d2d4ab677d974b08fce318ef902e
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-04-09 22:37:10 +0900

    Title: 최대 H 점수 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/최대 H 점수 2/maximum-h-score-2.cpp
+++ 260409/최대 H 점수 2/maximum-h-score-2.cpp
@@ -14,25 +14,30 @@ int main() {
     }
 
     int arr2[n] = {};
+    
     int max_count = 0;
     int result;
     //배열을 하나 복사하자. 하나하나 복구시키기엔 l값이 통제가 어렵다.
     for(int i = 1; i <= 100; i++) { //H값 완전탐색.
         copy(arr, arr+n, arr2);
+        bool index[n] = {};
         if(l > 0) { //만약 l이 0보다 크다면. 더해야한다. 1을 어디에, 얼마나?
         //h값과 가장 가까운 곳에. 근데 l은 여러개다. 그럼? 그냥 반복 시키자.
             for(int j = 0; j < l; j++) {
-                int min = 200;
                 int min_index = 0;
-                int now;
+                bool work = false;
+
                 for(int k = 0; k < n; k++) {
-                    now = abs(arr2[k] - i);
-                    if(min > now && arr2[k] != i) { //3이 기준이라고 하자. 그럼 1 100 2 3이면 3에 더해버린다. 당사자면 안된다.
-                        min = now;
+                    if(arr2[k] == i-1 && index[k] == 0) { //3이 기준이라고 하자. 그럼 1 100 2 3이면 3에 더해버린다. 당사자면 안된다.
                         min_index = k;
+                        work = true;
                     }
                 }
-                arr2[min_index] += 1;
+
+                if(work == true) {
+                    arr2[min_index] += 1;
+                    index[min_index] = 1;
+                }
             }
         }
 
