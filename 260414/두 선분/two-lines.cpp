674ed5c11523d94d9f3e154ee323350dce4047d0
daegyu10 <60037935+daegyu10@users.noreply.github.com>
2026-04-14 21:59:44 +0900

    Title: 두 선분, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260414/두 선분/two-lines.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x1, x2, x3, x4;
+    cin >> x1 >> x2 >> x3 >> x4;
+
+    if(x2 >= x3 || x4 >= x1 || (x1 <= x3 && x4 <= x2) || (x3 <= x1 && x2 <= x4)) {
+        cout << "intersecting";
+    } else {
+        cout << "nonintersecting";
+    }
+    return 0;
+}
